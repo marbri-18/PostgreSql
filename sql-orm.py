eb91e62c18e69f4abcb77d88255fd324193ac746
Martin Bridgman <martin.bridgman@gmail.com>
2022-02-22 14:06:53 +0000

    Set up table class structures

--- sql-orm.py
+++ sql-orm.py
@@ -8,6 +8,37 @@ from sqlalchemy.orm import sessionmaker
 db = create_engine("postgresql:///chinook")
 base = declarative_base()
 
+
+# Create a class base model for the "Artist" table
+class Artist(base):
+    __tablename__ = "Artist"
+    ArtistId = Column(Integer, primary_key=True),
+    Name = Column(String)
+)
+
+# Create a class base model for the "Album" table
+class Album(base):
+    __tablename__ = "Album"
+    AlbumId = Column(Integer, primary_key=True),
+    Title = Column(String),
+    ArtistId = Column(Integer, ForeignKey("Artist.ArtistId"))
+)
+
+# Create a class base model for the "Track" table
+class Track(base):
+    __tablename__ = "Track"
+    TrackId = Column(Integer, primary_key=True),
+    Name = Column(String),
+    AlbumId = Column(Integer, ForeignKey("Album.AlbumId")),
+    MediaTypeId = Column(Integer, primary_key=False),
+    GenreId = Column(Integer, primary_key=False),
+    Composer = Column(String),
+    Milliseconds = Column(Integer),
+    Bytes = Column(Integer),
+    UnitPrice = Column(Float)
+) 
+
+
 # Instead of connecting to the database directly, a session is requested
 # Create a new instance of sessionmaker, then point to the engine
 Session = sessionmaker(db)
@@ -16,3 +47,4 @@ session = Session()
 
 # Create the database using declarative-base subclass
 base.metadata.create_all(db)
+
